2a76241638bc664c370ebab19f4d0ada6de78da7
onworks14 <150022695+onworks14@users.noreply.github.com>
2024-01-28 10:45:19 +0000

    Add mp3 files

--- 1.sh
+++ 1.sh
@@ -1,3 +1,2 @@
-## wget -O ./audio/audio/开不了口.mp3 https://colddb.netlify.app/audio/开不了口.mp3
-cp ./audio/audio/开不了口.mp3 ./audio/audio/开不了口.webm
-cp ./audio/audio/开不了口.mp3 
+wget -O ./audio/audio/手放开.mp3 https://colddb.netlify.app/audio/手放开.mp3
+cp ./audio/audio/手放开.mp3 ./audio/audio/手放开.webm
